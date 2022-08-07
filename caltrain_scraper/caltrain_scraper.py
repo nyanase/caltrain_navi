7fc65d6d54a03fbab4197b027a2bb55d00f90b10
Nao Yanase <nao.yanase@wustl.edu>
2022-08-06 19:06:30 -0700

    scraper stores data as json

--- caltrain_scraper/caltrain_scraper.py
+++ caltrain_scraper/caltrain_scraper.py
@@ -1,31 +1,44 @@
+from asyncore import write
 from bs4 import BeautifulSoup
 import requests
+import json
 
+NAME_ROW = 0
+SERVICE_TYPE_ROW = 1
 START_COL = 2
+SETUP_ROWS = 2
+CALTRAIN_URL = \
+  "https://www.caltrain.com/schedules/pdf-schedules?active_tab=route_explorer_tab"
 
-def scrape_sched(sched, bound, weekday=True, stations=None):
+def ct_soup():
+  caltrain_page = requests.get(CALTRAIN_URL)
+  return BeautifulSoup(caltrain_page.content, "html.parser")
+
+def scrape_sched(sched, bound, stations=None, prev_trains=None):
   rows = sched.find_all("tr")
-  trains = setup_trains(rows[:2], bound, weekday)
-  stations = scrape_rows(rows[2:], trains, bound=bound, stations=stations)
-  print(stations[-1])
-  
+  trains = setup_trains(rows[:SETUP_ROWS], bound)
+  stations = scrape_rows(rows[SETUP_ROWS:], trains, bound=bound, stations=stations)
+  if prev_trains: trains = prev_trains + trains
   return trains, stations
 
-def setup_trains(rows, bound, weekday=True):
-  trains = []
-  train_nums_row, train_type_row = rows[0], rows[1]
+def setup_trains(rows, bound):
+  trains = [] 
+  train_nums_row, train_type_row = rows[NAME_ROW], rows[SERVICE_TYPE_ROW]
   for col in train_nums_row.find_all("td")[START_COL:]:
     trains.append({
       "name": col.get_text(), 
       "direction": bound, 
       "stations": {},
-      "weekday": weekday
+      "weekday": is_weekday_train(col) 
     })
   for i, col in enumerate(train_type_row.find_all("td")[START_COL:]):
     trains[i]["service_type"] = col.get_text() 
   return trains
 
-def scrape_rows(rows, trains, bound="nb", stations=None):
+def is_weekday_train(col):
+  return col["data-service-type"] == "weekday"
+
+def scrape_rows(rows, trains, bound="NB", stations=None):
   stations_ = [] if stations is None else stations
   rows = rows[::-1] if bound == "SB" else rows
   i = 0
@@ -34,18 +47,17 @@ def scrape_rows(rows, trains, bound="nb", stations=None):
       continue
     cols = row.find_all("td")
     station = setup_station(cols) if stations is None else stations_[i]
-    scrape_times(station, trains, cols, bound)
+    scrape_times(station, trains, cols)
     if stations is None: stations_.append(station)
     i += 1
   return stations_
 
-def scrape_times(station, trains, cols, bound="NB"):
-  tt_bound = "times_trains_nb" if bound == "NB" else "times_trains_sb"
+def scrape_times(station, trains, cols):
   for i, col in enumerate(cols[2:]):
     time = col.get_text()
     if time == "--":
       continue
-    station[f"{tt_bound}"].append((time, trains[i]["name"]))
+    station[f"times_trains"].append((time, trains[i]["name"]))
     trains[i]["stations"][f"{station['name']}"] = time
   return
 
@@ -53,19 +65,23 @@ def setup_station(cols):
   station = {}
   station["zone"] = cols[0].get_text()
   station["name"] = cols[1].get_text()
-  station["times_trains_nb"] = [] 
-  station["times_trains_sb"] = [] 
+  station["times_trains"] = [] 
   return station
 
-def main():
-  caltrain_url = \
-  "https://www.caltrain.com/schedules/pdf-schedules?active_tab=route_explorer_tab"
-  caltrain_page = requests.get(caltrain_url)
-  soup = BeautifulSoup(caltrain_page.content, "html.parser")
-  nb_sched, sb_sched = tuple(soup.find_all("table", class_="caltrain_schedule"))
-  nb_wkday_trains, stations = scrape_sched(nb_sched, bound="NB")
-  sb_wkday_trains, stations = scrape_sched(sb_sched, bound="SB", stations=stations)
+def write_to_file(filename, data):
+  with open(filename, 'w') as fp:
+    json.dump(data, fp)
+  return
 
+def main():
+  wkday_soup = ct_soup() 
+  nb_wkday_sched, sb_wkday_sched = tuple(wkday_soup.find_all("table",
+                                          class_="caltrain_schedule"))
+  trains, stations = scrape_sched(nb_wkday_sched, bound="NB")
+  trains, stations = scrape_sched(sb_wkday_sched, bound="SB", 
+                                  stations=stations, prev_trains=trains)
+  write_to_file("scraped_data/trains.json", trains)
+  write_to_file("scraped_data/stations.json", stations)
 
 if __name__ == "__main__":
   main()
